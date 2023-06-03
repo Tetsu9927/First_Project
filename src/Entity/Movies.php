5cf079a7cff60228535c83e829f8a833e8fc2c92
Tetsu99 <mariusz.oborski99@gmail.com>
2023-06-03 14:39:35 +0200

    First Project

---
+++ src/Entity/Movies.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MoviesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MoviesRepository::class)]
+class Movies
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
